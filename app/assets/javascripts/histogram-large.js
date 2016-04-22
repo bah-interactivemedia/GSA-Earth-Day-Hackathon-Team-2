5b0384a7122992ea085524b41286aa444d84b6bb
Ashish Tonse <ashish.tonse@gmail.com>
2016-04-22 14:01:38 -0400

    various things

--- app/assets/javascripts/histogram-large.js
+++ app/assets/javascripts/histogram-large.js
@@ -41,26 +41,4 @@
         var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
-
-      google.charts.setOnLoadCallback(drawPieChart);
-      function drawPieChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
-
-        var options = {
-          title: 'My Daily Activities'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-    }
 }());
-
